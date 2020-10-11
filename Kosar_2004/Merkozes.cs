c5f20e514cdbd2e34c1738b0d0f1c0a60d691f7a
Axmart <notaxmart@gmail.com>
2020-10-11 17:01:55 +0200

    Add project files.

---
+++ Kosar_2004/Merkozes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kosar_2004
+{
+    public class Merkozes
+    {
+        public string HazaiCsapat { get; set; }
+        public string IdegenCsapat { get; set; }
+        public int HazaiPont { get; set; }
+        public int IdegenPont { get; set; }
+        public string Helyszin { get; set; }
+        public string Idopont { get; set; }
+
+        public static List<Merkozes> beolvasas()
+        {
+            var sr = new StreamReader("../../eredmenyek.csv",Encoding.UTF8);
+            sr.ReadLine();
+            string[] temp;
+            var outputlist = new List<Merkozes>();
+            while (!sr.EndOfStream)
+            {
+                temp = sr.ReadLine().Split(';');
+                outputlist.Add(new Merkozes() { HazaiCsapat = temp[0], IdegenCsapat = temp[1], HazaiPont = int.Parse(temp[2]), IdegenPont = int.Parse(temp[3]), Helyszin = temp[4], Idopont = temp[5] });
+            }
+
+            sr.Close();
+
+            return outputlist;
+
+        }
+
+
+    }
+}
---
+++ Kosar_2004/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kosar_2004
+{
+    class Program
+    {
+
+        public static List<Merkozes> Merkozesek; 
+
+        static void Main(string[] args)
+        {
+            Merkozesek = Merkozes.beolvasas();
+
+            harmadikFeladat();
+            negyedikFeladat();
+            otodikFeladat();
+
+            Console.ReadKey();
+
+        }
+
+        private static void otodikFeladat()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void negyedikFeladat()
+        {
+
+            bool dontetlen = false;
+            int szamlalo = 0;
+
+            while(!dontetlen && szamlalo < Merkozesek.Count)
+            {
+
+                if (Merkozesek[szamlalo].HazaiPont == Merkozesek[szamlalo].IdegenPont)
+                {
+                    dontetlen = true;
+
+                }
+                else
+                {
+                    szamlalo++;
+                }
+
+            }
+
+            // LINQ megoldas
+            //dontetlen =  Merkozesek.Count(x => x.HazaiPont == x.IdegenPont)==0?false:true;
+
+            Console.WriteLine($"4. feladat : Volt dontetlen? {(dontetlen?"igen":"nem")}");
+
+            
+        }
+
+        private static void harmadikFeladat()
+        {
+
+            int hazaiMeccsekSzama = 0;
+            int idegenMeccsekSzama = 0;
+
+            foreach (var meccs in Merkozesek)
+            {
+               
+
+                if (meccs.HazaiCsapat.Equals("Real Madrid"))
+                {
+                    hazaiMeccsekSzama++;
+                }
+                else if (meccs.IdegenCsapat.Equals("Real Madrid"))
+                {
+                    idegenMeccsekSzama++;
+                }   
+
+        
+
+            }
+            
+            //LINQ megoldas
+            //hazaiMeccsekSzama = Merkozesek.Count(x => x.HazaiCsapat == "Real Madrid");
+            //idegenMeccsekSzama = Merkozesek.Count(x => x.IdegenCsapat == "Real Madrid");
+
+            Console.WriteLine($"3. feladat : Real Madrid: Hazai {hazaiMeccsekSzama}, Idegen {idegenMeccsekSzama}");
+
+
+        }
+    }
+}
